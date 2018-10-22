6e656ae8aeb7446e55b3633c8f8cd23f8aefbfc6
bharatbonde <bharat.bonde@gmail.com>
2018-10-21 21:31:29 -0400

    Adding centralised config service.

---
+++ configurationservice/src/main/java/org/bharat/application/starter/ConfigurationService.java
@@ -0,0 +1,13 @@
+package org.bharat.application.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationService {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationService.class, args);
+    }
+}
--- ms2/src/main/java/bharat/rest/endpoints/MS2.java
+++ ms2/src/main/java/bharat/rest/endpoints/MS2.java
@@ -2,16 +2,23 @@ package bharat.rest.endpoints;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class MS2 {
     private static final Logger log = LoggerFactory.getLogger(MS2.class);
+
+    @Value("${test.property:Hello default}")
+    private String message;
+
     @RequestMapping("/v1/ms2")
     public Starter operation(@RequestParam(value = "name", defaultValue = "Bharat") String nameVar) {
         log.info("IN MS2");
-        return new Starter(null, null, nameVar);
+        return new Starter(message, null, nameVar);
     }
 }
